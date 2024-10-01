fc0a3296bf48e00faf38a0ff250a63da3fe436b2
Gonzalo Viedma Diaz <166060031+GonzaloViedma@users.noreply.github.com>
2024-10-01 16:11:06 +0200

    Segunda actualizacion de java

---
+++ IrrgartenJava/Irrgarten/src/irrgarten/Irrgarten.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author gonzaalovd
+ */
+public class Irrgarten {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Weapon w1 = new Weapon(100,50);
+        String info;
+        
+        info = w1.toString();
+        System.out.println("Caracteristicas del arma:" + info);*/
+        //Directions dir = Directions.DOWN;
+        //System.out.println("La direccion es: " + dir);
+        for (int i = 0; i<10; i++){
+            System.out.println("Inteligencia: " + Dice.usesleft());
+        }
+    }
+    
+}
---
+++ IrrgartenJava/Irrgarten/src/irrgarten/Shield.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author gonzaalovd
+ */
+public class Shield {
+    private float protection;
+    private int uses;
+    
+    public Shield(float protection, int uses){
+        this.protection = protection;
+        this.uses = uses;
+    }
+    
+    public float protect(){
+        if(this.uses > 0){
+            this.uses--;
+            return this.protection;
+        }
+        return 0;
+    }
+    
+    public String toString(){
+        String cad;
+        cad = "S[" + this.protection + "," + this.uses + "]";
+        return cad;
+    }
+}
