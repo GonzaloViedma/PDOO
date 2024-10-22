0a8f31291c053ad703ee53ca08c1a19f6c3bd02b
Gonzalo Viedma Diaz <166060031+GonzaloViedma@users.noreply.github.com>
2024-10-22 16:31:49 +0200

    Clase Labyrinth

--- labyrinth.rb
+++ labyrinth.rb
@@ -14,10 +14,19 @@ module Irrgarten
       @n_cols = n_cols
       @exit_row = exit_row
       @exit_col = exit_col
+      #si pongo Array.new(4, 5), me hace un array de 4 con cincos. por eso me sirve para inicializar con empty char
+      @labyrinth = Array.new(@n_rows){Array.new(@n_cols, @@EMPTY_CHAR)}
+      @players = Array.new(@n_rows){Array.new(@n_cols, nil)}
+      @monsters = Array.new(@n_rows){Array.new(@n_cols, nil)}
+
+      @labyrinth[@exit_row][@exit_col] = @@EXIT_CHAR
     end
+
+    #atributos de relaciones
+
 
     #def have_a_winner
     # if
-        #end
+    # end
   end
 end
